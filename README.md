9dc65cf8f600677813b33b0ef0274237a93f17ab
Ben Powell <benpowdev@gmail.com>
2025-04-13 19:08:45 +0100

    Initial 2

---
+++ README.md
@@ -0,0 +1,3 @@
+# Schedule2
+
+Developed with Unreal Engine 5
---
+++ Source/Schedule2/Schedule2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Schedule2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Schedule2, "Schedule2" );
